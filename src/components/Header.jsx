17c065a0d06f456e07721a6abb4eaf5f6dcbd142
tinku kohad <tinkukohad05@gmail.com>
2024-05-02 12:55:28 +0530

    4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,67 +10,7 @@ const handleClick = () => {
 
   return (
     <>
-      {/* <section class="flex  bg-black items-center flex-wrap lg:justify-start gap-4 py-3 sm:px-10 px-4 border-gray-200 border-b min-h-[75px]">
-       
-        <a href="javascript:void(0)" class="shrink-0">
-          <img
-            src={logo}
-            alt="logo"
-            class="md:w-[170px] w-36"
-          />
-        </a>
-
-        <div class="lg:absolute lg:right-10 flex items-center ml-auto space-x-8">
-          <span class="relative">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              class="cursor-pointer fill-[#333] hover:fill-[#007bff] inline-block"
-              viewBox="0 0 64 64"
-            >
-              <path
-                d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                data-original="#000000"
-              />
-            </svg>
-            <span class="absolute left-auto -ml-1 top-0 rounded-full bg-black px-1 py-0 text-xs text-white">
-              1
-            </span>
-          </span>
-          <span class="relative">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              height="20px"
-              class="cursor-pointer fill-[#333] hover:fill-[#007bff] inline-block"
-              viewBox="0 0 512 512"
-            >
-              <path
-                d="M164.96 300.004h.024c.02 0 .04-.004.059-.004H437a15.003 15.003 0 0 0 14.422-10.879l60-210a15.003 15.003 0 0 0-2.445-13.152A15.006 15.006 0 0 0 497 60H130.367l-10.722-48.254A15.003 15.003 0 0 0 105 0H15C6.715 0 0 6.715 0 15s6.715 15 15 15h77.969c1.898 8.55 51.312 230.918 54.156 243.71C131.184 280.64 120 296.536 120 315c0 24.812 20.188 45 45 45h272c8.285 0 15-6.715 15-15s-6.715-15-15-15H165c-8.27 0-15-6.73-15-15 0-8.258 6.707-14.977 14.96-14.996zM477.114 90l-51.43 180H177.032l-40-180zM150 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm167 15c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm0 0"
-                data-original="#000000"
-              ></path>
-            </svg>
-            <span class="absolute left-auto -ml-1 top-0 rounded-full bg-black px-1 py-0 text-xs text-white">
-              4
-            </span>
-          </span>
-          <div class="inline-block cursor-pointer border-gray-300">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              height="20px"
-              viewBox="0 0 24 24"
-              class="hover:fill-[#007bff]"
-            >
-              <circle cx="10" cy="7" r="6" data-original="#000000" />
-              <path
-                d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
-                data-original="#000000"
-              />
-            </svg>
-          </div>
-        </div>
-      </section> */}
+     
 
       <header class="flex border-b border-1 bg-white font-sans min-h-[70px] tracking-wide relative z-50" style={{position:"fixed",width:"100%",top:"0",boxShadow:"2px 2px 4px rgba(0, 0, 0.4, 0.3)"}}>
 
@@ -95,6 +35,7 @@ const handleClick = () => {
               id="toggleClose"
               class="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3"
               onClick={handleClick}
+              style={{display:isMenuOpen?"block":"none"}}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -686,7 +627,8 @@ const handleClick = () => {
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
                 onClick={handleClick}
-                style={{display:"hidden"}}
+                style={{display:!isMenuOpen?"block":"none"}}
+
               >
                 <path
                   fill-rule="evenodd"
